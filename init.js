9f212fb6ab9f4587bb515422e95b47667ae6e51c
QnoraeT <xxajlopezxx@gmail.com>
2023-04-20 18:16:53 -0400

    fixes related to EB's advice

--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 "use strict";
-let canvasSize = {width: 0, height: 0}
+let canvasSize = {width: 0, height: 0};
 
 window.addEventListener('resize', resize);
     
@@ -9,111 +9,110 @@ function resize(){
         var height = window.innerHeight || document.body.clientHeight; 
         var ratio = height / h;
         var newWidth = (width / ratio);
-        canvasSize.width = newWidth
-        canvasSize.height = h
-        console.log(canvasSize)
+        canvasSize.width = newWidth;
+        canvasSize.height = h;
+        console.log(canvasSize);
 }
 
-var CamX = 0
-var CamY = 0
-var Zoom = 1
-var Shake = 0
-var ShakeDecay = 0.01
-var ShakeX = 0
-var ShakeY = 0
-var Time = 0
-var delta = 0
-var TimeSpeed = 1
-var damageList = []
-var damageIndicatorList = []
-var effectList = ["Burn", "Poison", "Sunstroke", "Paralyze", "Sleep", "Freeze", "Confusion", "Strange", "Crying", "Ichor", "Ironskin", "Light Shield", "Revitalize", "Energize", "Dazed", "Focus", "Taunt", "Slow", "Fast", "Unstable Magic", "Bad Poison", "Silence", "Strong", "Weak"]
-var musicVolume = 1.00
-var sfxVolume = 0.30
-var music = [new Audio('music/forget not (shortened).mp3'), new Audio('music/something with danidanijr V3.6 NO ARTS.mp3')]
-var comboSound = []
+let CamX = 0;
+let CamY = 0;
+let Zoom = 1;
+let Shake = 0;
+let ShakeDecay = 0.01;
+let ShakeX = 0;
+let ShakeY = 0;
+let Time = 0;
+let delta = 0;
+let TimeSpeed = 1;
+let damageList = [];
+let damageIndicatorList = [];
+let effectList = ["Burn", "Poison", "Sunstroke", "Paralyze", "Sleep", "Freeze", "Confusion", "Strange", "Crying", "Ichor", "Ironskin", "Light Shield", "Revitalize", "Energize", "Dazed", "Focus", "Taunt", "Slow", "Fast", "Unstable Magic", "Bad Poison", "Silence", "Strong", "Weak"];
+let musicVolume = 1.00;
+let sfxVolume = 0.30;
+let music = [new Audio('music/forget not (shortened).mp3'), new Audio('music/something with danidanijr V3.6 NO ARTS.mp3')];
+let comboSound = [];
 
-for (let i = 1; i <= 16; ++i){
-    comboSound.push(new Audio('sfx/combo_' + i + '.mp3'))
-    comboSound.push(new Audio('sfx/combo_' + i + '_power.mp3'))
+/*for (let i = 1; i <= 16; ++i){
+    comboSound.push(new Audio('sfx/combo_' + i + '.mp3'));
+    comboSound.push(new Audio('sfx/combo_' + i + '_power.mp3'));
 }
 
-var sfxMP3 = []
+let sfxMP3 = [];
 for (let i = 1; i <= 1; ++i){
-    comboSound.push(new Audio('sfx/sfx' + i + '.mp3'))
+    comboSound.push(new Audio('sfx/sfx' + i + '.mp3'));
 }
 
-var sfxWAV = []
+let sfxWAV = [];
 for (let i = 1; i <= 2; ++i){
-    comboSound.push(new Audio('sfx/sfx' + i + '.wav'))
-}
+    comboSound.push(new Audio('sfx/sfx' + i + '.wav'));
+}*/
 
-var musicState = 0
-let musicUpdate = 0
-var peopleNames = []
-var turnOrder = []
-var lastHP = []
+let musicState = 0;
+let musicUpdate = 0;
+let peopleNames = [];
+let turnOrder = [];
+let lastHP = [];
 
 for (let i = 0; i < music.length; ++i){
-    music[i].loop = true
+    music[i].loop = true;
 }
 
-class character {
+class Character {
     constructor(name, lvl, XPos, YPos, size, XOffsetHP, YOffsetHP, SizeOffsetHP, baseXPReq, xp, baseHP, baseMP, elem1, elem2, baseATK, baseDEF, baseSPD, hpType, personType, team, sizeX, sizeY){
-        this.name = name
-        let lf = lvl - 1
-        lf = (((lf + ((lf ** 2) / 15) + ((lf ** 3) / 120)) ** (1 + (lf / 250))) + 8) / 8
-        this.level = lvl
-        this.xPosition = XPos
-        this.yPosition = YPos
-        this.size = size
-        this.xPosHP = XOffsetHP
-        this.yPosHP = YOffsetHP
-        this.sHP = SizeOffsetHP
-        this.expPoints = xp
-        this.nextLv = baseXPReq * ( lf ** 1.5 )
-        this.maxHealth = baseHP * lf
-        this.health = this.maxHealth
-        this.maxMana = baseMP * lf
-        this.mana = this.maxMana
-        this.atk = baseATK * lf
-        this.def = baseDEF * lf
-        this.spd = baseSPD * ( lf ** 0.1 )
-        this.trueAtk = this.atk
-        this.trueDef = this.def
-        this.trueSpd = this.spd
-        this.HPtype = hpType
-        this.type1 = elem1
-        this.type2 = elem2
-        this.status = personType
-        this.team = team
-        this.target = ""
-        this.sEffects = []
-        this.sDuration = []
-        this.sStrength = []
-        this.spriteState = "Idle1"
-        this.lastSpriteState = "Idle1"
-        this.extraInfo = ["", "", 100] // who hit me?, who healed me?, my mood
-        peopleNames.push(this.name)
-        this.sizeX = sizeX
-        this.sizeY = sizeY
-        this.GigaPower = 0
-        this.GigaPowerMax = 100
-        this.HyperPower = 0
-        this.HyperPowerMax = 100
-        this.SuperPower = 0
-        this.SuperPowerMax = 100
-        this.hitTimer = 1
-        lastHP.push(this.health)
+        this.name = name;
+        let lf = lvl - 1;
+        lf = (((lf + ((lf ** 2) / 15) + ((lf ** 3) / 120)) ** (1 + (lf / 250))) + 8) / 8;
+        this.level = lvl;
+        this.xPosition = XPos;
+        this.yPosition = YPos;
+        this.size = size;
+        this.xPosHP = XOffsetHP;
+        this.yPosHP = YOffsetHP;
+        this.sHP = SizeOffsetHP;
+        this.expPoints = xp;
+        this.nextLv = baseXPReq * ( lf ** 1.5 );
+        this.maxHealth = baseHP * lf;
+        this.health = this.maxHealth;
+        this.maxMana = baseMP * lf;
+        this.mana = this.maxMana;
+        this.atk = baseATK * lf;
+        this.def = baseDEF * lf;
+        this.spd = baseSPD * ( lf ** 0.1 );
+        this.trueAtk = this.atk;
+        this.trueDef = this.def;
+        this.trueSpd = this.spd;
+        this.HPtype = hpType;
+        this.type1 = elem1;
+        this.type2 = elem2;
+        this.status = personType;
+        this.team = team;
+        this.target = "";
+        this.sEffects = [];
+        this.sDuration = [];
+        this.sStrength = [];
+        this.spriteState = "Idle1";
+        this.extraInfo = ["", "", 100] // who hit me?, who healed me?, my mood;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.GigaPower = 0;
+        this.GigaPowerMax = 100;
+        this.HyperPower = 0;
+        this.HyperPowerMax = 100;
+        this.SuperPower = 0;
+        this.SuperPowerMax = 100;
+        this.hitTimer = 1;
+        lastHP.push(this.health);
+        peopleNames.push(this.name);
     }
 
     updateSTATEffects(){
-        let mod
-        mod = 1
-        this.trueAtk = this.atk * mod
-        mod = 1
-        this.trueDef = this.def * mod
-        mod = 1
-        this.trueSpd = this.spd * mod * (Math.random() * 0.5) + 0.75
+        let mod;
+        mod = 1;
+        this.trueAtk = this.atk * mod;
+        mod = 1;
+        this.trueDef = this.def * mod;
+        mod = (Math.random() * 0.5) + 0.75;
+        this.trueSpd = this.spd * mod;
     }
 
     updateHPMPEffects(){
@@ -125,23 +124,24 @@ class character {
     }
 
     damage(person, attack, type){
-        damageList.push("Damage", this.name, person, attack, type)
+        damageList.push("Damage", this.name, person, attack, type);
     }
 
     giveStatusEffect(person, id, duration, strength, type){
-        damageList.push("Effect", this.name, person, id, duration, strength, type)
+        damageList.push("Effect", this.name, person, id, duration, strength, type);
     }
 }
 
 let people = {
-    "Alterian Skyler": new character("Alterian Skyler", 89, 0, 100, 1, -16.5, 20, 1, 45, 0, 150, 15, "Normal", "Electric", 10, 2, 16, "Normal", "PlayerBoss", 0, 128, 256),
-    //"ToWM_TowerSB": new character("ToWM_TowerSB", 70, -50, -50, 1, 0, 40, 1, 35, 0, 55, 10, "Normal", "", 25, 10, 16, "Normal", "Player", 1, 128, 256),
-    //"ToFUN_TowerSB": new character("ToFUN_TowerSB", 70, -30, -50, 1, 0, 40, 1, 40, 0, 50, 16, "Normal", "", 40, 20, 10, "Normal", "Player", 1, 128, 256),
-    //"Delet Ball": new character("Delet Ball", 1, 50, -50, 1, 0, 25, 1, 1e7, 0, 172554, 19886, "Dark", "", 27446, 10965, 31, "Normal", "Boss", 2, 256, 256)
+    "Alterian Skyler": new Character("Alterian Skyler", 1, 0, 100, 1, -16.5, 20, 1, 45, 0, 47, 11, "Normal", "Electric", 7, 2.3, 16, "Normal", "PlayerBoss", 0, 128, 256),
+    //"Alterian Skyler": new Character("Alterian Skyler", 89, 0, 100, 1, -16.5, 20, 1, 45, 0, 150, 15, "Normal", "Electric", 10, 2, 16, "Normal", "PlayerBoss", 0, 128, 256),
+    "ToWM:TowerSB": new Character("ToWM:TowerSB", 70, -50, -50, 1, 0, 40, 1, 35, 0, 55, 10, "Normal", "", 25, 10, 16, "Normal", "Player", 1, 128, 256),
+    "ToFUN:TowerSB": new Character("ToFUN:TowerSB", 70, -30, -50, 1, 0, 40, 1, 40, 0, 50, 16, "Normal", "", 40, 20, 10, "Normal", "Player", 1, 128, 256),
+    "Delet Ball": new Character("Delet Ball", 1, 50, -50, 1, 0, 25, 1, 1e7, 0, 172554, 19886, "Dark", "", 27446, 10965, 31, "Normal", "Boss", 2, 256, 256)
 }
 
 for (let i = 0; i < music.length; ++i){
-    music[i].load()
+    music[i].load();
 }
 
-resize()
+resize();
--- main.js
+++ main.js
@@ -1,230 +1,218 @@
 "use strict";
-var pos = []
+var pos = [];
 
 function allInQueue(){
-    let attempts = 0
+    let attempts = 0;
     for (let i = 0; i < damageList.length; ++attempts){
         if (attempts > 10000){
-            throw new Error("Hey! Something went wrong! I can't process this!")
+            throw new Error("Hey! Something went wrong! I can't process this!");
         }
         if (damageList[i] == "Damage"){
-            people[damageList[i+2]].extraInfo[0] = damageList[i+1]
-            let DMsub = 0
-            let DMdiv = 1
-            let DMpow = 1
-            let trueDamage = (Math.max((damageList[i+3] - DMsub), 0) ** DMpow) / DMdiv
-            people[damageList[i+2]].health = people[damageList[i+2]].health - trueDamage
-            people[damageList[i+2]].hitTimer = 0
-            i = i + 5
-            console.log(format(trueDamage, 3, 10000) + " damage")
+            people[damageList[i+2]].extraInfo[0] = damageList[i+1];
+            let DMsub = 0;
+            DMsub += people[damageList[i+2]].trueDef
+            let DMdiv = 1;
+            let DMpow = 1;
+            let trueDamage = (Math.max((damageList[i+3] - DMsub), 0) ** DMpow) / DMdiv;
+            people[damageList[i+2]].health = people[damageList[i+2]].health - trueDamage;
+            people[damageList[i+2]].hitTimer = 0;
+            if (people[damageList[i+2]].health <= 0) console.log("Mortal! [" + format(-1 * people[damageList[i+2]].health, 3, 1000000) + "] HP overkill");
+            console.log(format(trueDamage, 3, 1000000) + " damage");
+            i = i + 5;
         }    
         if (damageList[i] == "Effect"){
-            let pA = damageList[i+2].name // personAffected
-            let effectType = damageList[i+3]
-            let duration = damageList[i+4]
-            let strength = damageList[i+5]
+            let pA = damageList[i+2].name; // personAffected
+            let effectType = damageList[i+3];
+            let duration = damageList[i+4];
+            let strength = damageList[i+5];
             if (people[pA].sEffects.includes(effectType)){
-                let id = people[pA].sEffects.indexOf(effectType)
-                switch(damageList[i+6]) {
+                let id = people[pA].sEffects.indexOf(effectType);
+                switch(damageList[i+6]){
                     case 0: 
                         if (duration > people[pA].sDuration[id]){
-                            people[pA].sDuration[id] = duration
-                            console.log("effect duration increased")
+                            people[pA].sDuration[id] = duration;
+                            console.log("effect duration increased");
                         }
                         if (strength > people[pA].sStrength[id]){
-                            people[pA].sStrength[id] = strength
-                            console.log("effect strength increased")
+                            people[pA].sStrength[id] = strength;
+                            console.log("effect strength increased");
                         }
-                        console.log("effect stats checked")
+                        console.log("effect stats checked");
                         break
                     case 1: 
-                        people[pA].sDuration[id] = duration
-                        people[pA].sStrength[id] = strength
-                        console.log("effect stats overwritten")
+                        people[pA].sDuration[id] = duration;
+                        people[pA].sStrength[id] = strength;
+                        console.log("effect stats overwritten");
                         break
                     case 2:
-                        console.log("already has one. nothing changed")
+                        console.log("already has one. nothing changed");
                         break
                     default:
-                        throw new Error("What do you want me to do here?? " + people[pA].name + " already has the effect " + effectList[effectType])
+                        throw new Error("What do you want me to do here?? " + people[pA].name + " already has the effect " + effectList[effectType]);
                 }
             } else {
-                people[pA].sEffects.push(effectType)
-                people[pA].sDuration.push(duration)
-                people[pA].sStrength.push(strength)
+                people[pA].sEffects.push(effectType);
+                people[pA].sDuration.push(duration);
+                people[pA].sStrength.push(strength);
             }
-            i = i + 7
+            i = i + 7;
         }    
     }
-    damageList = []
+    damageList = [];
 }
 
 function clamp(num, min, max){ // why isn't this built in
-    return Math.min(Math.max(num, min), max)
+    return Math.min(Math.max(num, min), max);
 }
 
 function lerp(t, s, e, type, p){
-    t = clamp(t, 0, 1)
+    t = clamp(t, 0, 1);
     if (t == 0){
-        return s
+        return s;
     }
     if (t == 1){
-        return e
+        return e;
     }
     switch(type) {
         case "QuadIn": 
-            t = t * t
-            break
+            t = t * t;
+            break;
         case "QuadOut": 
-            t = 1.0 - ((1.0 - t) * (1.0 - t))
-            break
+            t = 1.0 - ((1.0 - t) * (1.0 - t));
+            break;
         case "CubeIn": 
-            t = t * t * t
-            break
+            t = t * t * t;
+            break;
         case "CubeOut": 
-            t = 1.0 - ((1.0 - t) * (1.0 - t) * (1.0 - t))
-            break
+            t = 1.0 - ((1.0 - t) * (1.0 - t) * (1.0 - t));
+            break;
         case "Smooth": 
-            t = 6 * (t ** 5) - 15 * (t ** 4) + 10 * (t ** 3)
-            break
+            t = 6 * (t ** 5) - 15 * (t ** 4) + 10 * (t ** 3);
+            break;
         case "ExpSCurve": 
-            t =  (Math.tanh(p * Math.tan((t + 1.5 - ((t - 0.5) / 1e9)) * Math.PI)) + 1) / 2
-            break
+            t =  (Math.tanh(p * Math.tan((t + 1.5 - ((t - 0.5) / 1e9)) * Math.PI)) + 1) / 2;
+            break;
         case "Expo": 
         if (p > 0) {
-            t = Math.coth(p / 2)*Math.tanh(p * t / 2)
+            t = Math.coth(p / 2)*Math.tanh(p * t / 2);
         } else if (p < 0) {
-            t = 1.0 - Math.coth(p / 2)*Math.tanh(p * (1.0 - t) / 2)
+            t = 1.0 - Math.coth(p / 2)*Math.tanh(p * (1.0 - t) / 2);
         } 
-            break
+            break;
         default:
-            break
+            break;
     }
-    return (s*(1-t))+(e*t)
+    return (s*(1-t))+(e*t);
 }
 
 function newMusic(m){
+    if (m >= music.length){
+        throw new Error('Hey! That song is out of bounds! ' + m);
+    }
     for (let i = 0; i < music.length; ++i){
-        music[i].load()
+        console.log(i)
+        music[i].load();
     }
-    if (m > music.length){
-        throw new Error('Hey! That song is out of bounds! ' + m)
-    }
-    music[m].play()
+    music[m].play();
 }
 
 function updateVisuals(){
-    let sprite
-    let dlastHP
-    let dcurrHP
-    for (let i = 0; i < peopleNames.length; ++i){
-        dcurrHP = clamp(people[peopleNames[i]].health / people[peopleNames[i]].maxHealth,0,1)
+    let dlastHP;
+    let dcurrHP;
+    for (let i = 0; i < 1; ++i){
+        dcurrHP = clamp(people[peopleNames[i]].health / people[peopleNames[i]].maxHealth,0,1);
+        dlastHP = clamp(lastHP[i] / people[peopleNames[i]].maxHealth,0,1);
         translateXY(people[peopleNames[i]].xPosition,
             people[peopleNames[i]].yPosition, 
             people[peopleNames[i]].sizeX, 
             people[peopleNames[i]].sizeY
-            )
-        const character = document.getElementById('character' + i);
-        character.style.left = pos[0] + 'px'
-        character.style.top = pos[1] + 'px'
-        character.style.width = pos[2] + 'px'
-        character.style.height = pos[3] + 'px'
+            );
+        changeAtt('character' + i, pos[0], pos[1], pos[2], pos[3], "", "characters/Alterian Skyler/assets/" + people[peopleNames[i]].spriteState + ".svg")
         // update HP bar
         translateXY(
             people[peopleNames[i]].xPosition + people[peopleNames[i]].xPosHP,
             people[peopleNames[i]].yPosition + people[peopleNames[i]].yPosHP, people[peopleNames[i]].sizeX * people[peopleNames[i]].sHP, 8 * people[peopleNames[i]].sHP)
-        const hpContainer = document.getElementById('hp' + i + '-container');
-        hpContainer.style.left = pos[0] + 'px'
-        hpContainer.style.top = pos[1] + 'px'
-        hpContainer.style.width = pos[2] + 'px'
-        hpContainer.style.height = pos[3] + 'px'
-        const hpFill3 = document.getElementById('hp' + i + 'c'); // empty
-        hpFill3.style.left = pos[0] + 'px'
-        hpFill3.style.top = pos[1] + 'px'
-        hpFill3.style.width = pos[2] + 'px'
-        hpFill3.style.height = pos[3] + 'px'
-        hpFill3.style.backgroundColor = getRainbowColour(2 * dcurrHP,0.25,1)
-        const hpFill = document.getElementById('hp' + i + 'b'); // last
-        hpFill.style.left = pos[0] + 'px'
-        hpFill.style.top = pos[1] + 'px'
-        hpFill.style.width = clamp(lastHP[i] / people[peopleNames[i]].maxHealth,0,1) * pos[2] + 'px'
-        hpFill.style.height = pos[3] + 'px'
-        hpFill.style.backgroundColor = getRainbowColour(1,0.9,(Math.sin(24*Time)/2)+0.5)
-        const hpFill2 = document.getElementById('hp' + i + 'a'); // fill
-        hpFill2.style.left = pos[0] + 'px'
-        hpFill2.style.top = pos[1] + 'px'
-        hpFill2.style.width = dcurrHP * pos[2] + 'px'
-        hpFill2.style.height = pos[3] + 'px'
-        hpFill2.style.backgroundColor = getRainbowColour(2 * dcurrHP,1,1)
-
+        changeAtt('hp' + i + '-container', pos[0], pos[1], dcurrHP * pos[2], pos[3])
+        changeAtt('hp' + i + 'c', pos[0], pos[1], pos[2], pos[3], gRC(2 * dcurrHP, 0.25, 1))
+        changeAtt('hp' + i + 'b', pos[0], pos[1], dlastHP * pos[2], pos[3], gRC(1, 0.9, (Math.sin(24 * Time) / 2) + 0.5))
+        changeAtt('hp' + i + 'a', pos[0], pos[1], dcurrHP * pos[2], pos[3], gRC(2 * dcurrHP, 1, 1))
     }
+}
+
+function changeAtt(spriteID, left, top, width, height, bgColor, src){
+    const sprite = document.getElementById(spriteID)
+    if (!(left === "" || left === undefined)) sprite.style.left = left + 'px';
+    if (!(top === "" || top === undefined)) sprite.style.top = top + 'px';
+    if (!(width === "" || width === undefined)) sprite.style.width = width + 'px';
+    if (!(height === "" || height === undefined)) sprite.style.height = height + 'px';
+    if (!(bgColor === "" || bgColor === undefined)) sprite.style.backgroundColor = bgColor;
+    if (!(src === "" || src === undefined)) sprite.src = src;
 }
 
 function updateLastHP(){
     for (let i = 0; i < peopleNames.length; ++i){
-        people[peopleNames[i]].hitTimer = people[peopleNames[i]].hitTimer + delta
+        people[peopleNames[i]].hitTimer = people[peopleNames[i]].hitTimer + delta;
         if (people[peopleNames[i]].hitTimer >= 1){
             if (lastHP[i] - people[peopleNames[i]].health > people[peopleNames[i]].maxHealth * delta * 0.25){
-                lastHP[i] = lastHP[i] + people[peopleNames[i]].maxHealth * delta * 0.25 * ((lastHP[i] > people[peopleNames[i]].health) ? -1 : 1)
+                lastHP[i] = lastHP[i] + people[peopleNames[i]].maxHealth * delta * 0.25 * ((lastHP[i] > people[peopleNames[i]].health) ? -1 : 1);
             } else {
-                lastHP[i] = people[peopleNames[i]].health
+                lastHP[i] = people[peopleNames[i]].health;
             }
 
         }
     }
 }
 
-function getRainbowColour(time, val, sat){
-    let r = 0
-    let g = 0
-    let b = 0
-    let t = time % 1
-    let s = Math.floor(time) % 6
+function gRC(time, val, sat){
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    let t = time % 1;
+    let s = Math.floor(time) % 6;
     switch(s) {
         case 0: 
-            r = 1
-            g = t
-            break
+            r = 1;
+            g = t;
+            break;
         case 1: 
-            r = 1 - t
-            g = 1
-            break
+            r = 1 - t;
+            g = 1;
+            break;
         case 2: 
-            g = 1
-            b = t
-            break
+            g = 1;
+            b = t;
+            break;
         case 3: 
-            g = 1 - t
-            b = 1
-            break
+            g = 1 - t;
+            b = 1;
+            break;
         case 4: 
-            b = 1
-            r = t
-            break
+            b = 1;
+            r = t;
+            break;
         case 5: 
-            b = 1 - t
-            r = 1
-            break
+            b = 1 - t;
+            r = 1;
+            break;
         default:
-            throw new Error("Unexpected value!!")
+            throw new Error("Unexpected value!!");
     }
-    r = 1 - ((1 - r) * sat)
-    g = 1 - ((1 - g) * sat)
-    b = 1 - ((1 - b) * sat)
-    r = r * val * 255
-    g = g * val * 255
-    b = b * val * 255
+    r = 1 - ((1 - r) * sat);
+    g = 1 - ((1 - g) * sat);
+    b = 1 - ((1 - b) * sat);
+    r = r * val * 255;
+    g = g * val * 255;
+    b = b * val * 255;
     return "#" + pad(Math.round(r).toString(16), 2) 
                + pad(Math.round(g).toString(16), 2) 
-               + pad(Math.round(b).toString(16), 2)
+               + pad(Math.round(b).toString(16), 2);
 }
 
 function pad(num,length){
-    while(num.length < length)
-    {
-        num = "0" + num
+    while(num.length < length){
+        num = "0" + num;
     }
-    return num
+    return num;
 }
 
 function translateXY(x,y,xs,ys){
